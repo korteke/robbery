30a096ececc9b17577de18de3633b3d9c6683a85
Keijo Korte <keijo.korte@kvak.net>
2016-01-31 21:31:11 +0200

    Modified message

--- src/main/java/net/kvak/SahkoBot/SahkoBot.java
+++ src/main/java/net/kvak/SahkoBot/SahkoBot.java
@@ -1,34 +1,37 @@
 package net.kvak.SahkoBot;
 
+import java.text.DecimalFormat;
+import java.util.concurrent.ThreadLocalRandom;
+
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 
-import java.text.DecimalFormat;
-import java.util.concurrent.ThreadLocalRandom;
-
 public class SahkoBot {
-    
-	// Espoo
+
+	// Caruna Espoo
 	private final static double PRICE_ESP = 0.0271;
 	private final static double ROBBERY_PRICE_ESP = 0.0314;
 	private final static double ROBBERY_MONTHLY_ESP = 5.90 - 2.95;
-	// Muu maa
+	// Caruna
 	private final static double PRICE = 0.0295;
 	private final static double ROBBERY_PRICE = 0.0398;
 	private final static double ROBBERY_MONTHLY = 22.60 - 15.67;
+	// Limits
 	private final static int MIN_KWH = 1000;
 	private final static int MAX_KWH = 30000;
 
 	public static void main(String[] args) throws TwitterException {
 		final int kwh = ThreadLocalRandom.current().nextInt(MIN_KWH, MAX_KWH + 1);
 		Twitter twitter = TwitterFactory.getSingleton();
-		
-        try {
-    		Status status = twitter.updateStatus("Yleissiirron hinnankorotus vuosikulutuksella: " + kwh + "kWh on Espoossa " + calculateRobbery(kwh, ROBBERY_PRICE_ESP, PRICE_ESP, ROBBERY_MONTHLY_ESP)
-    		+ "€/vuosi. Muualla suomessa "+calculateRobbery(kwh, ROBBERY_PRICE, PRICE, ROBBERY_MONTHLY)+"€/vuosi. @CarunaSuomi #kohtuullista");
-    		System.out.println("Posted tweet: " +status.getText() + " ID: " + status.getId());
+
+		try {
+			Status status = twitter.updateStatus("Espoossa yleissiirron vuotuinen korotus kulutuksella: " + kwh
+					+ "kWh on " + calculateRobbery(kwh, ROBBERY_PRICE_ESP, PRICE_ESP, ROBBERY_MONTHLY_ESP)
+					+ "€. Muualla Suomessa " + calculateRobbery(kwh, ROBBERY_PRICE, PRICE, ROBBERY_MONTHLY)
+					+ "€. #caruna #sähko #kohtuullista #ryöstö");
+			System.out.println("Posted tweet: " + status.getText() + " ID: " + status.getId());
 		} catch (Exception e) {
 			System.out.println(e);
 		}
